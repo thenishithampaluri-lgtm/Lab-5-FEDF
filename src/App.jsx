139afacd9ece27cbee448c79dc4169655385a882
thenishithampaluri-lgtm <thenishithampaluri@gmail.com>
2026-02-23 10:38:32 +0530

    before depleoy

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import AuthContainer from "./components/AuthContainer";
+import './index.css'
+function App() {
+  return (
+    <div>
+      <AuthContainer />
+    </div>
+  );
+}
+
+export default App;
